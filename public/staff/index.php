23f86e22ee4b3ba1dc576132b619a5e75475a532
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-05-17 14:46:04 -0500

    Add basic page layout (header, nav, footer) and verify php function correctly

---
+++ public/staff/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=\, initial-scale=1.0">
+  <title>GBI</title>
+</head>
+<body>
+  <header>
+    <h1>GBI Staff Area</h1>
+  </header>
+
+  <nav>
+    <ul>
+      <li>
+        <a href="index.php">Menu</a>
+      </li>
+    </ul>
+  </nav>
+
+  <footer>
+    &copy; G_BANK <?php echo date('Y'); ?>
+  </footer>
+</body>
+</html>
