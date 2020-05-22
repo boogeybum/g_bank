be0a4e41a95a8e70bc2ec9c15e25f910792a5128
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-05-22 15:44:16 -0500

    Create urlencode and rawurlencode functions then require and use in show.php

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,3 +1,6 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+
 <?php
 
 $id = $_GET['id'] ?? '1'; //if statement to return default id of 1 if no id sent
@@ -5,3 +8,7 @@ $id = $_GET['id'] ?? '1'; //if statement to return default id of 1 if no id sent
 echo $id;
 
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br>
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br>
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br>
