e33ebeeff17c164097328b46f157e5d4dd3fe6ee
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-05-22 16:34:33 -0500

    Add link from staff/index.php to pages/index.php and config basic layout on pages/index.php

---
+++ public/staff/pages/index.php
@@ -0,0 +1,29 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<!-- list of $pages -->
+
+<?php $page_title = 'Pages'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+  <div class="pages">
+    <h1>Pages</h1>
+
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Position</th>
+      <th>Visible</th>
+      <th>Name</th>
+      <th>&nbsp;</th>
+      <th>&nbsp;</th>
+      <th>&nbsp;</th>
+    </tr>
+    
+    <!-- insert foreach() to loop through pages -->
+
+  </table>
+  </div>
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
