f8002944a19dae1cbeacff94b43e02c370e80331
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-05-22 17:51:25 -0500

    Create link back to pages/index.php and page id text

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -1,10 +1,23 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
+<?php $page_title = 'Show Page'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-<?php
-  $id = $_GET['id'] ?? '1';
+<div id="content">
+  
+  <div class="">
+    <a class="action" href="<?php echo url_for('/staff/pages/index.php'); ?>">&#8678; Back to List</a>
+  </div>
 
-  echo h($id);
 
-?>
+  Page ID: 
+  <?php
+    $id = $_GET['id'] ?? '1';
+    echo h($id);
+  ?>
+  
 
+</div>
+
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
