75e3b1d42bbb4050a96ea7d143bd0235084820c4
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-05-22 17:06:58 -0500

    Add comment for three strings for demonstrating urlencode use cases

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -9,6 +9,7 @@ echo h($id);
 
 ?>
 
+<!-- strings for demonstrating urlencode use cases -->
 <a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br>
 <a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br>
 <a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br>
