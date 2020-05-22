abb80c16dba28ec54a13dcd45c36c203b615378f
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-05-22 17:14:41 -0500

    Change pages dummy list to match tutorial

--- public/staff/pages/index.php
+++ public/staff/pages/index.php
@@ -3,10 +3,10 @@
 <!-- dummy list of $pages -->
 <?php
   $pages = [
-    ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'About Globe Bank'],
-    ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'Consumer'],
-    ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'SMall Business'],
-    ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Commercial']
+    ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'Globe Bank'],
+    ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'History'],
+    ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Leadership'],
+    ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Contact Us']
   ];
 ?>
 
