9c4ebbcd7f9071d1c0606fabf63e9c39219cc624
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-05-22 20:57:57 -0500

    Add layout to subjects/show.php with links

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,15 +1,32 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
 
-<?php
+<?php $page_title = 'Show Subjects'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-$id = $_GET['id'] ?? '1'; //if statement to return default id of 1 if no id sent
+<div id="content">
 
-echo h($id);
+  <div>
+    <a class="action" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&#8678; Back to List</a>
+  
+  </div>
 
-?>
 
-<!-- strings for demonstrating urlencode use cases -->
-<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br>
-<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br>
-<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br>
+  Page ID:
+  <!-- if statement to return default id of 1 if no id sent -->
+  <?php
+  $id = $_GET['id'] ?? '1'; 
+  echo h($id);
+  ?>
+
+  <!-- strings for demonstrating urlencode use cases -->
+  <a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br>
+  <a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br>
+  <a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br>
+
+</div>
+
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
+
+
