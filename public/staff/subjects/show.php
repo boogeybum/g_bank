9648f01e5ab755d8b7e026e98d2bc20e97e77b6e
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-05-22 21:24:41 -0500

    Refactor to match tutorial and remove urlencode and rawurlencode example lines

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,34 +1,23 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
+<?php
+  $id = $_GET['id'] ?? '1';
+?>
 
 <?php $page_title = 'Show Subjects'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
 
-  <div>
     <a class="action" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&#8678; Back to List</a>
   
-  </div>
+  <div class="subjects show" >
 
-
-  Page ID:
-  <!-- if statement to return default id of 1 if no id sent -->
-  <?php
-  $id = $_GET['id'] ?? '1'; 
-  echo h($id);
-  ?>
-
-  <div>
-    <!-- strings for demonstrating urlencode use cases -->
-    <a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br>
-    <a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br>
-    <a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br>
+    <!-- if statement to return default id of 1 if no id sent -->
+    Page ID: <?php echo h($id); ?>
+  
   </div>
 
 </div>
 
-
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
-
-
