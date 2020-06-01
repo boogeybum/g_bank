ead2d757b06792117ca5d924a61d2beca483a3ed
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-06-01 17:08:40 -0500

    Define default values for form values

--- public/staff/pages/new.php
+++ public/staff/pages/new.php
@@ -5,4 +5,10 @@ require_once('../../../private/initialize.php');
 if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/pages/index.php'));
 }
+
+$id = $_GET['id'];
+$menu_name = '';
+$position = '';
+$visible = '';
+
 ?>
