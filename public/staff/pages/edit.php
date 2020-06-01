6c173a9920f5a8e7cae7a205ced6ca01a5b6621f
boogeybum <45719473+boogeybum@users.noreply.github.com>
2020-06-01 18:11:53 -0500

    Set redirect if id is not set. Add default form values. Echo form values when post request.

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -2,6 +2,27 @@
 
 require_once('../../../private/initialize.php');
 
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/staff/pages/index.php'));
+}
+$id = $_GET['id'];
+$menu_name = '';
+$position = '';
+$visible = '';
+
+if(is_post_request()) {
+
+  // Handle form values sent and return values at top of page
+
+  $menu_name = $_POST['menu_name'] ?? '';
+  $position = $_POST['position'] ?? '';
+  $visible = $_POST['visible'] ?? '';
+
+  echo "Form parameters<br />";
+  echo "Menu name: " . $menu_name . "<br />";
+  echo "Position: " . $position . "<br />";
+  echo "Visible: " . $visible . "<br />";
+}
 ?>
 
 <?php $page_title = 'Edit Page'; ?>
@@ -37,7 +58,7 @@ require_once('../../../private/initialize.php');
         <input type="submit" value="Edit Page">
       </div>
     </form>
-    
+
   </div>
 
 </div>
